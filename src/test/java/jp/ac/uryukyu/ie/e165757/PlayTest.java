0c4f9dc46bc112b95bbbde0911b388cfcea8023f
Shota Nagamine <e165757@ie.u-ryukyu.ac.jp>
2017-02-07 04:30:59 +0900

    Report7

---
+++ src/test/java/jp/ac/uryukyu/ie/e165757/PlayTest.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.e165757;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by e165757 on 2017/02/07.
+ */
+public class PlayTest {
+    @Test
+    public void getPlace() throws Exception {
+
+    }
+
+    @Test
+    public void getName() throws Exception {
+
+    }
+
+}
